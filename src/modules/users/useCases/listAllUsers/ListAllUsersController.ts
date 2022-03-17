5215188cda48cf267d6c62f253445e1a33f8aabc
Arthur Freitas <cesarinoarthur@gmail.com>
2022-03-17 11:48:20 -0300

    ListAllUsers useCase complete

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const userId = request.header("user_id")
+
+    const allUsers = this.listAllUsersUseCase.execute({ user_id: userId })
+
+    return response.json(allUsers)
   }
 }
 
